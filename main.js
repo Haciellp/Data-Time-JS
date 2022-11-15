5ed81d14a13faa4cc369c97546af0d2cb917d3be
Haciellp <108480136+Haciellp@users.noreply.github.com>
2022-11-15 11:37:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let monstrarFecha = document.getElementById('fecha');
+let monstrarReloj = document.getElementById('reloj');
+
+let fecha = new Date();
+
+let diaSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles',
+ 'Jueves', 'Viernes', 'Sabado'];
+
+let mesAnyo = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo',
+ 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre',
+ 'Diciembre'];
+
+ monstrarFecha.innerHTML = `${diaSemana[fecha.getDay()]}, 
+ ${fecha.getDate()} de ${mesAnyo[fecha.getMonth()]} de ${fecha.getFullYear()}`
+ 
+ setInterval(() =>{
+    let hora = new Date();
+    monstrarReloj.innerHTML = hora.toLocaleTimeString();
+ }, 1000);
